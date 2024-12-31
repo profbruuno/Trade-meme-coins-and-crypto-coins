447db53642c878415897f10eecc3aad9ebdf01a7
profbruuno <96679152+profbruuno@users.noreply.github.com>
2024-12-31 11:52:35 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// script.js
+document.querySelectorAll('.social-media-icons a').forEach(icon => {
+    icon.addEventListener('click', () => {
+        alert('Opening social media link...');
+    });
+});
 
+document.querySelectorAll('nav button a').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Opening social media link...');
+    });
+});
